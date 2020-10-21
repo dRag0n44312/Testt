a2c9dac0b93d50b9b7fac6806c170d22e49fcb5a
dRag0n44312 <aryan44312@gmail.com>
2020-10-21 20:40:29 +0530

    Angry

--- sketch.js
+++ sketch.js
@@ -19,6 +19,7 @@ function setup(){
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
+    box6 = new Box(200,200,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
@@ -45,6 +46,7 @@ function draw(){
     console.log(box2.body.angle);
     box1.display();
     box2.display();
+    box6.display();
     ground.display();
     pig1.display();
     log1.display();
